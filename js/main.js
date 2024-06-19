d85b7683ded3bc2e39a08235a0dd492d707b3899
JazzMedo <84742298+Jazzmedo@users.noreply.github.com>
2024-06-19 08:37:46 +0300

    finalizing welcome page not yet done

--- js/main.js
+++ js/main.js
@@ -8,3 +8,17 @@ document.addEventListener("DOMContentLoaded", function() {
       toggleContent.style.height = isOpen ? "0" : `${toggleContent.scrollHeight}px`;
   });
 });
+
+var divs = document.querySelectorAll('.con');
+
+function handleClick(event) {
+    divs.forEach(function (div) {
+        div.classList.remove('clicked');
+    });
+
+    event.target.classList.add('clicked');
+}
+
+divs.forEach(function (div) {
+    div.addEventListener('click', handleClick);
+});
