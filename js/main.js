5942837b3c769870382acb498b89e5171ffd88bc
JazzMedo <84742298+Jazzmedo@users.noreply.github.com>
2024-06-20 07:28:47 +0300

    email subscribe

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const toggleButton = document.getElementById('toggleButton');
-  const toggleContent = document.getElementById('toggleContent');
+document.addEventListener("DOMContentLoaded", function () {
+    const toggleButton = document.getElementById('toggleButton');
+    const toggleContent = document.getElementById('toggleContent');
 
-  toggleButton.addEventListener('click', function() {
-      const isOpen = toggleButton.textContent.includes("LESS");
-      toggleButton.textContent = isOpen ? "EXPLORE MORE" : "EXPLORE LESS";
-      toggleContent.style.height = isOpen ? "0" : `${toggleContent.scrollHeight}px`;
-  });
+    toggleButton.addEventListener('click', function () {
+        const isOpen = toggleButton.textContent.includes("LESS");
+        toggleButton.textContent = isOpen ? "EXPLORE MORE" : "EXPLORE LESS";
+        toggleContent.style.height = isOpen ? "0" : `${toggleContent.scrollHeight}px`;
+    });
 });
 
 var divs = document.querySelectorAll('.con');
